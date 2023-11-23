875b0e11483e17fa924cb6a9ac4d3b7751cdba15
joaomarcelo-franca <joao.franca@al.infnet.edu.br>
2023-11-23 15:43:21 -0300

    primeiro commit logs

--- src/main/java/br/com/infnet/LogsUtil/Logs.java
+++ src/main/java/br/com/infnet/LogsUtil/Logs.java
@@ -1,2 +1,20 @@
-package br.com.infnet.LogsUtil;public class Logs {
+package br.com.infnet.LogsUtil;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Logs {
+    public static void main(String[] args) {
+        Logger LOGGER = LoggerFactory.getLogger(Logs.class);
+        LOGGER.info("Iniciando a Main!");
+
+        try{
+            LOGGER.debug("Comecando Divisao");
+            LOGGER.debug("Divindo 10 por 2: " + 10/2);
+            LOGGER.debug("Divindo 10 por 0: " + 10/0);
+        } catch (Exception ex){
+            LOGGER.error("situacao de erro " + ex.getMessage());
+        }
+
+    }
 }
